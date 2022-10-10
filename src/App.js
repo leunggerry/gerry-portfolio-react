827012ebe5d45ac67b177497195941dfa4274105
gigi.upNorth <leung.gerry@gmail.com>
2022-10-10 14:34:53 -0400

    [#1 Nav] Header section adding active tag to current tab view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
-
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
 
 function App() {
+  const [tabs] = useState(["about", "portfolio", "contact", "resume"]);
+  // create states for the tab that is being set
+
+  const [currentTab, setTab] = useState(tabs[0]);
   return (
     <div>
-      <Nav />
+      <Nav currentTab={currentTab} setTab={setTab} />
       <main>
         <About />
       </main>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,52 @@
 import React from "react";
 
 // Component Definition
-function Nav() {
+function Nav(props) {
+  // Destructure the props coming in
+  const { currentTab, setTab } = props;
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand mb-0 h1" href="/">
             Gerry Leung
           </a>
 
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#about">
+              <a
+                className={`nav-link ${currentTab === "about" && `active`}`}
+                href="#about"
+                onClick={() => setTab("about")}
+              >
                 About Me
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#portfolio">
+              <a
+                className={`nav-link ${currentTab === "portfolio" && `active`}`}
+                href="#portfolio"
+                onClick={() => setTab("portfolio")}
+              >
                 Portfolio
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#contact">
+              <a
+                className={`nav-link ${currentTab === "Contact" && `active`}`}
+                href="#contact"
+                onClick={() => setTab("contact")}
+              >
                 Contact
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#resume">
+              <a
+                className={`nav-link ${currentTab === "Resume" && `active`}`}
+                href="#resume"
+                onClick={() => setTab("resume")}
+              >
                 Resume
               </a>
             </li>
