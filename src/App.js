061cf62dd36a222707794e0004624863e0d04222
gigi.upNorth <leung.gerry@gmail.com>
2022-10-11 22:31:19 -0400

    [Resume Section] Adding Technology stack and button for resume

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import ContactForm from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   // create states for the tab that is being set
   const [tabs] = useState(["about", "portfolio", "contact", "resume"]);
   //set the background color
-  const [bgColor, setBgColor] = useState("#6f6b5e");
+  const [bgColor] = useState("#6f6b5e");
   document.body.style.backgroundColor = bgColor;
   // set the state of the tab
   const [currentTab, setTab] = useState(tabs[0]);
+
+  //set the tech stack
+  const frontEndStack = ["HTML", "CSS", "MaterializeCSS", "Bootstrap", "JQuery", "React"];
+  const backEndStack = ["NodeJS", "MongoDB", "Mongoose", "Express", "MySQL", "NoSQL"];
   return (
     <div>
       <Nav currentTab={currentTab} setTab={setTab} />
@@ -24,6 +29,9 @@ function App() {
 
         {/* show the contact form */}
         {currentTab === "contact" && <ContactForm />}
+
+        {/* show the resume section */}
+        {currentTab === "resume" && <Resume front={frontEndStack} backend={backEndStack} />}
       </main>
       <footer>
         <Footer />
